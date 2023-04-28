9608c5a4578602b86c75fe7230a0dc67eafed24e
tomi <tomiwest@hotmail.com>
2023-04-28 12:03:55 +0300

    setup script to build client and copy files to server folder

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT | 3001
 
-app.get('/', (req, res) => {
+app.use(express.static('dist'))
+
+app.get('/api', (req, res) => {
   res.send('Hello World!')
 })
 
