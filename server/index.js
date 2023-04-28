b878f39259057fab8c42e332e7e8943850aa624a
tomi <tomiwest@hotmail.com>
2023-04-28 13:08:04 +0300

    create app file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,5 @@
-const express = require('express')
-const app = express()
+const app = require('./app')
 const PORT = process.env.PORT || 3001
-
-app.use(express.static('dist'))
-
-app.get('/api', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
