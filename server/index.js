552b289217478b629093b6a41a38a9957c080d59
tomi <tomiwest@hotmail.com>
2023-04-28 13:09:02 +0300

    create utils folder with config.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const PORT = process.env.PORT || 3001
+const { PORT } = require('./utils/config')
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
