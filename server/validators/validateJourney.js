1987e95322c6540fcb093dc03dfbd25cd6095cf4
tomi <tomiwest@hotmail.com>
2023-05-02 08:27:11 +0300

    validateJourney replace all values with constants

--- server/validators/validateJourney.js
+++ server/validators/validateJourney.js
@@ -1,55 +1,63 @@
 const MIN_STATION_ID = 1
 const MIN_JOURNEY_DURATION = 600
 const MIN_COVERED_DISTANCE = 10
-function validateJourney(row) {
-  const validKey = [
-    'Covered distance (m)',
-    'Departure station id',
-    'Departure station name',
-    'Duration (sec.)',
-    'Return',
-    'Return station id',
-    'Return station name',
-    'Departure',
-  ]
 
-  const trimmed = Object.fromEntries(
+const COVERED_DISTANCE = 'Covered distance (m)'
+const DEPARTURE_DATE = 'Departure'
+const RETURN_DATE = 'Return'
+const DEPARTURE_STATION_ID = 'Departure station id'
+const JOURNEY_DURATION = 'Duration (sec.)'
+
+const validKeys = [
+  'Covered distance (m)',
+  'Departure station id',
+  'Departure station name',
+  'Duration (sec.)',
+  'Return',
+  'Return station id',
+  'Return station name',
+  'Departure',
+]
+
+function validateJourney(row) {
+  const trimmedRow = Object.fromEntries(
     Object.entries(row).map(([key, value]) => [key.trim(), value])
   )
-  const rowKeys = Object.keys(trimmed)
 
-  if (!rowKeys.every((field) => validKey.includes(field))) {
+  if (!Object.keys(trimmedRow).every((field) => validKeys.includes(field))) {
     return false
   }
 
   if (
-    isNaN(trimmed[validKey[0]]) ||
-    trimmed[validKey[0]] < MIN_COVERED_DISTANCE
+    isNaN(trimmedRow[COVERED_DISTANCE]) ||
+    trimmedRow[COVERED_DISTANCE] < MIN_COVERED_DISTANCE
   ) {
     return false
   }
 
   if (
-    isNaN(Date.parse(trimmed['Departure'])) ||
-    isNaN(Date.parse(trimmed['Return']))
-  ) {
-    return false
-  }
-
-  if (Date.parse(trimmed['Departure']) > Date.parse(trimmed['Return'])) {
-    return false
-  }
-
-  if (
-    isNaN(trimmed['Departure station id']) ||
-    parseInt(trimmed['Departure station id']) < MIN_STATION_ID
+    isNaN(Date.parse(trimmedRow[DEPARTURE_DATE])) ||
+    isNaN(Date.parse(trimmedRow[RETURN_DATE]))
   ) {
     return false
   }
 
   if (
-    isNaN(trimmed['Duration (sec.)']) ||
-    trimmed['Duration (sec.)'] < MIN_JOURNEY_DURATION
+    Date.parse(trimmedRow[DEPARTURE_DATE]) > Date.parse(trimmedRow[RETURN_DATE])
+  ) {
+    return false
+  }
+
+  if (
+    isNaN(trimmedRow[DEPARTURE_STATION_ID]) ||
+    parseInt(trimmedRow[DEPARTURE_STATION_ID]) < MIN_STATION_ID
+  ) {
+    return false
+  }
+
+  if (
+    isNaN(trimmedRow[JOURNEY_DURATION]) ||
+    trimmedRow[JOURNEY_DURATION] < MIN_JOURNEY_DURATION
   )
     return false
 
