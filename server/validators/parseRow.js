b71278e3ccf9dfe9d3b817270395d9bd0b5ce71b
tomi <tomiwest@hotmail.com>
2023-05-03 10:49:38 +0300

    update parseRow

--- server/validators/parseRow.js
+++ server/validators/parseRow.js
@@ -1,10 +1,12 @@
 const {
   convertSpaceToUnderscore,
   removeParethesis,
+  removeDot,
 } = require('../utils/helpers')
 
 function parseRow(row) {
-  return convertSpaceToUnderscore(removeParethesis(row))
+  const rowWithoutDots = removeDot(row)
+  return convertSpaceToUnderscore(removeParethesis(rowWithoutDots))
 }
 
 module.exports = parseRow
