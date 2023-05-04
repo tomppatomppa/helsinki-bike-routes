cc27b5cac63c2d26df6fe6b032106d2ceeea25ce
tomi <tomiwest@hotmail.com>
2023-05-04 10:22:08 +0300

    add tests for query params offset

--- server/__test__/journeysQueryValidator.test.js
+++ server/__test__/journeysQueryValidator.test.js
@@ -35,4 +35,27 @@ describe('journeysQueryValidator', () => {
       await request(app).get('/api/journeys').query({ limit: 10 }).expect(200)
     })
   })
+  describe('Test offset', () => {
+    test('should return 400 when offset is not a positive integer -1', async () => {
+      await request(app).get('/api/journeys').query({ offset: -1 }).expect(400)
+    })
+    test('should return 400 when offset is a string', async () => {
+      await request(app)
+        .get('/api/journeys')
+        .query({ offset: 'string' })
+        .expect(400)
+    })
+    test('should return 400 when offset a float', async () => {
+      await request(app).get('/api/journeys').query({ offset: 2.2 }).expect(400)
+    })
+    test('should return 200 when offset is a number string', async () => {
+      await request(app)
+        .get('/api/journeys')
+        .query({ offset: '20' })
+        .expect(200)
+    })
+    test('should return 200 when offset is 0', async () => {
+      await request(app).get('/api/journeys').query({ offset: 0 }).expect(200)
+    })
+  })
 })
