8b63c0b76d358b12f2697a16f742e71fd6b82c0f
tomi <tomiwest@hotmail.com>
2023-05-08 15:33:46 +0300

    tests for useQueryParams hook

--- client/src/__tests__/useQueryParams.test.tsx
+++ client/src/__tests__/useQueryParams.test.tsx
@@ -1,11 +1,64 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
 import { describe, expect, test } from 'vitest'
 
 import useQueryParams from '../hooks/useQueryParams'
 
 describe('useQueryParams hook', () => {
-  test('Should start with limit 0 by default', () => {
+  test('Should start with limit 20 by default', () => {
     const { result } = renderHook(() => useQueryParams())
-    expect(result.current.queryParams.limit).toBe(0)
+    expect(result.current.queryParams.limit).toBe(20)
+  })
+  test('Should start with offset 0 by default', () => {
+    const { result } = renderHook(() => useQueryParams())
+    expect(result.current.queryParams.offset).toBe(0)
+  })
+  test('Should start with order ["",""] by default', () => {
+    const { result } = renderHook(() => useQueryParams())
+    expect(result.current.queryParams.order[0]).toBe('')
+    expect(result.current.queryParams.order[1]).toBe('')
+  })
+  test('Should start with search "" by default', () => {
+    const { result } = renderHook(() => useQueryParams())
+    expect(result.current.queryParams.search).toBe('')
+  })
+  test('Should start with search_field "" by default', () => {
+    const { result } = renderHook(() => useQueryParams())
+    expect(result.current.queryParams.search_field).toBe('')
+  })
+  test('Should set order to ASC and then DESC', () => {
+    const { result } = renderHook(() => useQueryParams())
+    act(() => {
+      result.current.orderByColumn('Name')
+    })
+    expect(result.current.queryParams.order).toEqual(['Name', 'ASC'])
+    act(() => {
+      result.current.orderByColumn('Name')
+    })
+    expect(result.current.queryParams.order).toEqual(['Name', 'DESC'])
+  })
+  test('Should reset order to ASC when column changes', () => {
+    const { result } = renderHook(() => useQueryParams())
+    act(() => {
+      result.current.orderByColumn('Nimi')
+    })
+    expect(result.current.queryParams.order).toEqual(['Nimi', 'ASC'])
+    act(() => {
+      result.current.orderByColumn('Name')
+    })
+    expect(result.current.queryParams.order).toEqual(['Name', 'ASC'])
+  })
+  test('Should set search_field', () => {
+    const { result } = renderHook(() => useQueryParams())
+    act(() => {
+      result.current.findByField('Osoite')
+    })
+    expect(result.current.queryParams.search_field).toEqual('Osoite')
+  })
+  test('Should set search', () => {
+    const { result } = renderHook(() => useQueryParams())
+    act(() => {
+      result.current.setSearch('Haka')
+    })
+    expect(result.current.queryParams.search).toEqual('Haka')
   })
 })
--- client/src/hooks/useQueryParams.tsx
+++ client/src/hooks/useQueryParams.tsx
@@ -1,9 +1,26 @@
 import { useState } from 'react'
 const useQueryParams = () => {
-  const [limit, setLimit] = useState<number>(0)
+  const [offset, setOffset] = useState<number>(0)
+  const [limit, setLimit] = useState<number>(20)
+  const [order, setOrder] = useState<string[]>(['', ''])
+  const [search, setSearch] = useState<string>('')
+  const [search_field, setSearchField] = useState('')
 
-  const queryParams = { limit }
-  return { queryParams }
+  const orderByColumn = (value: string | undefined) => {
+    if (!value) return
+    setOrder((prev) => {
+      const isNew = value === prev[0] ? false : true
+      if (isNew) return [value, 'ASC']
+      return [value, prev[1] === 'ASC' ? 'DESC' : 'ASC']
+    })
+  }
+
+  const findByField = (value: string) => {
+    setSearchField(value)
+  }
+
+  const queryParams = { limit, offset, order, search, search_field }
+  return { queryParams, orderByColumn, findByField, setSearch }
 }
 
 export default useQueryParams
