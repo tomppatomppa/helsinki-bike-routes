2c48f323eadfc4d6687a522947946c7733c6dee4
tomi <tomiwest@hotmail.com>
2023-05-23 10:19:07 +0300

    Lazy load infiniteScrollStations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,32 @@
+import { lazy, Suspense } from 'react'
 import InfiniteScrollStations from './components/stations/InfiniteScrollStations'
-import InfiniteScrollJourneys from './components/journeys/infiniteScrollJourneys'
 
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './components/layout/MainLayout'
+import Spinner from './components/common/Spinner'
+
+const InfiniteScrollJourneys = lazy(
+  () => import('./components/journeys/infiniteScrollJourneys')
+)
 
 function App() {
   return (
     <div className="App mx-auto text-center">
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/journeys" element={<InfiniteScrollJourneys />} />
-          <Route path="/stations" element={<InfiniteScrollStations />} />
-          <Route path="/" element={<InfiniteScrollStations />} />
-        </Route>
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="flex justify-center">
+            <Spinner show delay={500} />
+          </div>
+        }
+      >
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/journeys" element={<InfiniteScrollJourneys />} />
+            <Route path="/stations" element={<InfiniteScrollStations />} />
+            <Route path="/" element={<InfiniteScrollStations />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   )
 }
