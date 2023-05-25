43e6a12ab6a5e0a066a1d69d4c4b6cec242dc70f
tomi <tomiwest@hotmail.com>
2023-05-25 12:10:22 +0300

    set modal z-index to 50

--- client/src/components/common/Modal.tsx
+++ client/src/components/common/Modal.tsx
@@ -5,7 +5,7 @@ interface ModalProps {
 
 const Modal = ({ show, children }: ModalProps) => {
   return show ? (
-    <div className="fixed inset-0 w-screen h-screen backdrop-blur-sm overflow-auto bg-black bg-opacity-20 flex justify-center items-center">
+    <div className="fixed z-50 inset-0 w-screen h-screen backdrop-blur-sm overflow-auto bg-black bg-opacity-20 flex justify-center items-center">
       <div className="mx-auto max-w-3xl items-center mt-96 sm:mt-0">
         {children}
       </div>
